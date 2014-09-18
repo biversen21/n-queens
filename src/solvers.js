cd303c3a66479fc855d7175f10f6ac6560200f15
biversen21 <biversen21@gmail.com>
2014-09-17 17:26:40 -0700

    Begin working on n-Rooks solution

--- src/solvers.js
+++ src/solvers.js
@@ -1,3 +1,4 @@
+
 /*           _
    ___  ___ | |_   _____ _ __ ___
   / __|/ _ \| \ \ / / _ \ '__/ __|
@@ -15,8 +16,34 @@
 
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
+  // Start decision tree
+  var currentRow = [];
+  var idx = 0;
+  var board = [];
+  var buildLevel = function(){
+    idx = 0;
+    currentRow = [];
+    n = 3;
+    for (var i=0; i<n; i++){
+      if (i === idx){
+        currentRow.push(1);
+      } else {
+        currentRow.push(0);
+      }
+    }
+    board.push(currentRow);
 
+    console.log(board);
+  };
+  //   First level  = Generate n starting boards in array with seq. placement of rook
+  //   Iterate over array of first level boards
+  //     Generate second level of n boards for each first level board
+  //     Repeat iteration of second level... --> n
+  //   When n = this.rows.length traverse each n-level (completed) board
+  //     Run col and row check for each board
+  //     Return first board to pass checks
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  buildLevel();
   return solution;
 };
 
